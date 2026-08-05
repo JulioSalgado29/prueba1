f6edb435174760e18e88acacf06c91036cced392
Julio Salgado <51071954+JulioSalgado29@users.noreply.github.com>
2026-08-05 17:05:38 -0500

    -> Agregar crud de dueno_muestra

--- index.js
+++ index.js
@@ -14,45 +14,100 @@ app.get('/', (req, res) => {
   res.json({ mensaje: 'API corriendo localmente' });
 });
 
-// Listar dueno_muestra
-app.get('/api/dueno_muestra', async (req, res) => {
-  try {
-    const resultado = await pool.query
-    (`SELECT 
-      id_dueno_muestra,
-      email_usuario,
-      estado,
-      fecha_creacion,
-      id_inventario,
-      nombre,
-      usuario_creacion
-    FROM dueno_muestra
-    ORDER BY id_dueno_muestra DESC`);
-    res.json(resultado.rows);
-  } catch (error) {
-    console.error('Error en GET /api/dueno_muestra:', error.message);
-    res.status(500).json({ error: 'Error interno del servidor' });
-  }
-});
-
-app.post('/api/dueno_muestra', async (req, res) => {
-  const { email_usuario, fecha_creacion, id_inventario, nombre, usuario_creacion } = req.body;
-
-  const emailLimpio = email_usuario.trim().toLowerCase();
-
-  try {
-    const nuevo = await pool.query(
-      'INSERT INTO dueno_muestra (email_usuario, fecha_creacion, id_inventario, nombre, usuario_creacion) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-      [emailLimpio, fecha_creacion, id_inventario, nombre, usuario_creacion]
+//#region dueno_muestra
+  // Listar dueno_muestra
+  app.get('/api/dueno_muestra/:id_inventario', async (req, res) => {
+    const { id_inventario } = req.params;
+    try {
+      const resultado = await pool.query(
+      `SELECT 
+        id_dueno_muestra,
+        email_usuario,
+        estado,
+        fecha_creacion,
+        id_inventario,
+        nombre,
+        usuario_creacion
+      FROM dueno_muestra
+      WHERE estado = true AND 
+            id_inventario = $1
+      ORDER BY nombre DESC`,
+      [id_inventario]
     );
+      res.json(resultado.rows);
+    } catch (error) {
+      console.error('Error en GET /api/dueno_muestra:', error.message);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
 
-    res.status(201).json(nuevo.rows[0]);
+  //Insertar dueno_muestra
+  app.post('/api/dueno_muestra', async (req, res) => {
+    const { email_usuario, id_inventario, nombre, usuario_creacion } = req.body;
 
-  } catch (error) {
-    console.error('Error en POST /api/dueno_muestra:', error.message);  
-    res.status(500).json({ error: 'Error interno del servidor' });
-  }
-});
+    const emailLimpio = email_usuario.trim().toLowerCase();
+
+    try {
+      const nuevo = await pool.query(
+        'INSERT INTO dueno_muestra (email_usuario, id_inventario, nombre, usuario_creacion) VALUES ($1, $2, $3, $4) RETURNING *',
+        [emailLimpio, id_inventario, nombre, usuario_creacion]
+      );
+
+      res.status(201).json(nuevo.rows[0]);
+
+    } catch (error) {
+      console.error('Error en POST /api/dueno_muestra:', error.message);  
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
+
+  //Editar dueno_muestra
+  app.put('/api/dueno_muestra/:id', async (req, res) => {
+    const { id } = req.params;
+    const { email_usuario, nombre, usuario_creacion } = req.body;
+
+    const emailLimpio = email_usuario.trim().toLowerCase();
+
+    try {
+      const resultado = await pool.query(
+        'UPDATE dueno_muestra SET email_usuario = $1, nombre = $2, usuario_creacion = $3 WHERE id_dueno_muestra = $4 RETURNING *',
+        [emailLimpio, nombre, usuario_creacion, id]
+      );
+
+      if (resultado.rows.length === 0) {
+        return res.status(404).json({ error: 'dueno_muestra no encontrado' });
+      }
+
+      res.json(resultado.rows[0]);
+
+    } catch (error) {
+      console.error('Error en PUT /api/dueno_muestra/:id:', error.message);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
+
+  //Eliminar dueno_muestra
+  app.delete('/api/dueno_muestra/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const resultado = await pool.query(
+        'UPDATE dueno_muestra SET estado = $2 WHERE id_dueno_muestra = $1 RETURNING *',
+        [id, 'false']
+      );
+
+      if (resultado.rows.length === 0) {
+        return res.status(404).json({ error: 'dueno_muestra no encontrado' });
+      }
+
+      res.json({ mensaje: 'dueno_muestra eliminado correctamente' });
+
+    } catch (error) {
+      console.error('Error en DELETE /api/dueno_muestra/:id:', error.message);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
+//#endregion dueno_muestra
 
 /*
 // Expresión regular para validar formato de correo electrónico
