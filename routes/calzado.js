67988f014e3045d0de505ee604cdb1fac82f9512
Julio Salgado <51071954+JulioSalgado29@users.noreply.github.com>
2026-08-07 15:06:01 -0500

    -> el listado deberá ser por orden en el nombre

--- routes/calzado.js
+++ routes/calzado.js
@@ -25,7 +25,7 @@ router.get('/inventario/:id_inventario', async (req, res) => {
       FROM calzado
       WHERE activo = true AND 
             id_inventario = $1
-      ORDER BY fecha_creacion DESC`,
+      ORDER BY nombre DESC`,
             [id_inventario]
         );
         res.json(resultado.rows);
